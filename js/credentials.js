e959aa0a0a28e781b679359dbef1a2a5e2f5df37
James Taylor <jamest@uk.ibm.com>
2017-07-24 16:59:12 +0100

    Add query parameters for workspace details

--- js/credentials.js
+++ js/credentials.js
@@ -4,10 +4,15 @@
 var CredentialsPanel = (function() {
   // Publicly accessible methods defined
   return {
+    init: init,
     getCredentials: getCredentials
   };
 
   // Initialize the module
+  function init() {
+    processQueryParams();
+  }
+
   function getCredentials() {
     var workspaceId = document.getElementById('workspaceInput').value;
     var username = document.getElementById('usernameInput').value;
@@ -22,5 +27,30 @@ var CredentialsPanel = (function() {
     }
 
     return undefined;
+  }
+
+  function setCredentials(credentials) {
+    if (credentials.workspaceId) {
+      document.getElementById('workspaceInput').value = credentials.workspaceId;
+    }
+
+    if (credentials.username) {
+      document.getElementById('usernameInput').value = credentials.username;
+    }
+
+    if (credentials.password) {
+      document.getElementById('passwordInput').value = credentials.password;
+    }
+  }
+
+  function processQueryParams() {
+    var urlParams = new URLSearchParams(window.location.search);
+    var credentials = {};
+
+    credentials.username = urlParams.get('username');
+    credentials.password = urlParams.get('password');
+    credentials.workspaceId = urlParams.get('workspaceId');
+
+    setCredentials(credentials);
   }
 }());
--- js/global.js
+++ js/global.js
@@ -5,6 +5,7 @@
 // Other JS files required to be loaded first: apis.js, conversation.js, payload.js
 (function() {
   // Initialize all modules
+  CredentialsPanel.init();
   ConversationPanel.init();
   PayloadPanel.init();
 })();
